eafced978eeaf927a02849c34cfc1c939a6855bd
samirsouzasys <samir.guitar@gmail.com>
2022-04-05 00:41:58 -0300

    CardMain - OK

--- src/components/CardMain/index.tsx
+++ src/components/CardMain/index.tsx
@@ -1,7 +1,8 @@
-import { CardHeader } from "../CardHeader";
 import { Container, Content } from "./styles";
+import img_team from '../../assets/img_team.png';
 
-export function CardMain(props:any) {
+
+export function CardMain(props: any) {
 
   return (
     <>
@@ -9,13 +10,32 @@ export function CardMain(props:any) {
         <Content>
           <div className="main-left">
             <h1>
-              Welcome to<br/>your dashboard
+              Welcome to<br />your dashboard
             </h1>
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque quibusdam aliquam animi. Tempora quae ex voluptatem vero, iusto in natus praesentium corporis temporibus commodi itaque possimus accusantium quaerat officia quisquam?
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque quibusdam aliquam animi. Tempora quae ex voluptatem vero, iusto in natus praesentium corporis temporibus commodi itaque possimus accusantium quaerat officia quisquam?
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque quibusdam aliquam animi. Tempora quae ex voluptatem vero, iusto in natus praesentium corporis temporibus commodi itaque possimus accusantium quaerat officia quisquam?
             </p>
+          </div>
+          <div className="main-right">
+            <div className="img">
+              <img src={img_team} alt="Img Team" />
+            </div>
+            <div className="number-texts">
+              <div>
+                <div className="number">101</div>
+                <div className="texts">NEW<br />LEADS</div>
+              </div>
+              <div>
+                <div className="number">35</div>
+                <div className="texts">QUOTES<br />CREATED</div>
+              </div>
+              <div>
+                <div className="number">40</div>
+                <div className="texts">PENDING<br />ORDERS</div>
+              </div>
+            </div>
           </div>
         </Content>
       </Container>
--- src/components/CardMain/styles.ts
+++ src/components/CardMain/styles.ts
@@ -16,18 +16,58 @@ export const Container = styled.header`
   `
 
 export const Content = styled.div`
+  display: flex;
+  padding: 1.5rem 0;
+
   .main-left {
-    width:33%;
+    width:37%;
     padding: 1rem;
     color:white;
 
     h1 {
-      font-size: 4rem;
+      font-size: 3rem;
       font-weight: 800;
-      margin-bottom: 1.5rem;
+      margin-bottom: 2.4rem;
+    }
+    
+    p{
+      font-size: 1.2rem;
+      font-weight: 500;
+      text-align: justify;
     }
   }
   .main-right {
-    width:33%;
+    width: 63%;
+    
+    .img{
+      text-align:center;
+      margin: 1rem 0;
+    }
+  }
+  .number-texts{
+      text-align:center;
+      display: flex;
+
+      div {
+        display: flex;
+        margin: auto;
+      }
+
+      .number {
+        font-weight: 900;
+        font-family: sans-serif;
+        font-size: 5rem;
+        margin-right: 0.7rem;
+        color: #F0CE84;
+        text-shadow: 1px 1px 8px rgba(0,0,0,0.35);
+      }
+      
+      .texts {
+        font-size: 1.7rem;
+        color: white;
+        font-weight: 700;
+        text-align: left;
+        text-shadow: 1px 1px 8px rgba(0,0,0,0.2);
+      }
   }
 `
